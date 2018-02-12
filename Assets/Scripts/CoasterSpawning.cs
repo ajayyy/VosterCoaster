5fba7aa51a809b235fb33e397d76b90c0d7361d5
= <ajay@ajayinkingston.com>
2018-02-11 20:47:30 -0500

    Added a mini preview beside the controller, and added an input for the right and left trigger press (in the input manager, it does nothing so far)

---
+++ Assets/Scripts/CoasterSpawning.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoasterSpawning : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        print(Input.GetAxis("RightTrigger") + " " + Input.GetAxis("LeftTrigger"));
+	}
+}
