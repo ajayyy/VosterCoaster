b79772313ec6cfc3bb5644229f9d8cdad80e9ed5
= <ajay@ajayinkingston.com>
2018-02-13 16:07:03 -0500

    Made it spawn a new roller coaster when you press trigger with the right controller. The roller coaster falls and has a collider. Also added the ground.

--- Assets/Scripts/CoasterSpawning.cs
+++ Assets/Scripts/CoasterSpawning.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 
 public class CoasterSpawning : MonoBehaviour {
 
+    public GameObject coaster;
+
+    //the coaster connected to the controller
+    public GameObject thumbnailCoaster;
+
+    public GameObject rightController;
+
+    //did they just hit spawn
+    bool lastspawned;
+
 	void Start () {
 		
 	}
 	
 	void Update () {
-        print(Input.GetAxis("RightTrigger") + " " + Input.GetAxis("LeftTrigger"));
+        //print(Input.GetAxis("RightTrigger") + " " + Input.GetAxis("LeftTrigger"));   ,
+
+        if(Input.GetAxis("RightTrigger") == 1 && !lastspawned) {
+            lastspawned = true;
+
+            GameObject newCoaster = Instantiate(coaster);
+
+            newCoaster.transform.parent = newCoaster.transform.root;
+
+            newCoaster.transform.position = thumbnailCoaster.transform.position + Vector3.up * 0;
+            newCoaster.transform.rotation = thumbnailCoaster.transform.rotation;
+
+        } else if(Input.GetAxis("RightTrigger") != 1) {
+            lastspawned = false;
+        }
 	}
 }
