ec5cf67726030ad90f2b14e58d85dd26a21bc796
= <ajay@ajayinkingston.com>
2018-02-13 21:21:32 -0500

    Figured out how to get touchpad input, turns out you have to use the old steamvr plugin way as unity's default uses the joystick

--- Assets/Scripts/CoasterSpawning.cs
+++ Assets/Scripts/CoasterSpawning.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CoasterSpawning : MonoBehaviour {
 
-    public GameObject coaster;
+    //the coasters connected to the controller that you scroll through
+    public GameObject[] options;
 
-    //the coaster connected to the controller
-    public GameObject thumbnailCoaster;
+    //the current coaster scrolled to
+    int currentCoaster = 0;
 
     public GameObject rightController;
+
+    //for getting inputs
+    public SteamVR_TrackedController rightTracking;
 
     //did they just hit spawn
     bool lastspawned;
@@ -19,20 +23,31 @@ public class CoasterSpawning : MonoBehaviour {
 	}
 	
 	void Update () {
+
+        GameController gameController = GameController.instance;
+
         //print(Input.GetAxis("RightTrigger") + " " + Input.GetAxis("LeftTrigger"));   ,
 
         if(Input.GetAxis("RightTrigger") == 1 && !lastspawned) {
             lastspawned = true;
 
-            GameObject newCoaster = Instantiate(coaster);
+            GameObject newCoaster = Instantiate(options[currentCoaster]);
 
             newCoaster.transform.parent = newCoaster.transform.root;
 
-            newCoaster.transform.position = thumbnailCoaster.transform.position + Vector3.up * 0;
-            newCoaster.transform.rotation = thumbnailCoaster.transform.rotation;
+            newCoaster.transform.position = rightController.transform.position + Vector3.up * 0;
+            newCoaster.transform.eulerAngles = rightController.transform.eulerAngles + Vector3.up * 90; //the thumbnail is rotated 90 degrees from the controller
 
         } else if(Input.GetAxis("RightTrigger") != 1) {
             lastspawned = false;
         }
-	}
+
+        if (Input.GetButton("RightTrackpadClick")) {
+            print("sdasdsad");
+        } else {
+            print(gameController.rightController.GetAxis());
+        }
+
+
+    }
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+    public SteamVR_TrackedObject leftControllerTrackedObject;
+    public SteamVR_TrackedObject rightControllerTrackedObject;
+
+    public SteamVR_Controller.Device leftController {
+        get {
+            return SteamVR_Controller.Input((int)leftControllerTrackedObject.index);
+        }
+    }
+    public SteamVR_Controller.Device rightController {
+        get {
+            return SteamVR_Controller.Input((int)rightControllerTrackedObject.index);
+        }
+    }
+
+    void Start () {
+		if(instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
