e14f6e1cfa2f02426b4bfba594c968f5b87ca265
= <ajay@ajayinkingston.com>
2018-02-25 15:51:37 -0500

    Added 3D helper text

---
+++ Assets/Scripts/Util/TextOpacityChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOpacityChanger : MonoBehaviour {
+
+    public float opacity;
+
+    public Material material;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+        if(material.color.a != opacity) {
+            material.color = new Color(material.color.r, material.color.g, material.color.b, opacity);
+        }
+    }
+}
