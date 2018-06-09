9e0ac587e6a710984222a552ea3fc2c70ef1f851
= <ajay@ajayinkingston.com>
2018-06-09 14:15:27 -0400

    added math for turning, still not perfect yet, and only turns right

--- Assets/Scripts/AddIncline.cs
+++ Assets/Scripts/AddIncline.cs
@@ -10,7 +10,7 @@ public class AddIncline : MonoBehaviour {
     //array of all of the parents of the rail bones. This will be set by the inspector
     public GameObject[] railParents = new GameObject[3];
 
-    Vector3 adjustmentAngle = new Vector3(4.5f, 0, 0); //the number in the x represents the total angle the whole track rotates divided by 10
+    Vector3 adjustmentAngle = new Vector3(0, 10, 0); //the number in the x represents the total angle the whole track rotates divided by 9 (first bone does not have an angle
 
     void Start() {
         //create the rails array from the railParents
@@ -35,7 +35,7 @@ public class AddIncline : MonoBehaviour {
         }
 
         for (int i = 0; i < rails.Length; i++) {
-            for (int r = 0; r < rails[i].Length; r++) {
+            for (int r = 1; r < rails[i].Length; r++) {
                 //Attempt to rotate them all
                 rails[i][r].transform.localEulerAngles += adjustmentAngle;
             }
@@ -52,6 +52,22 @@ public class AddIncline : MonoBehaviour {
                 Vector3 pos = rails[i][r].transform.position;
 
                 rails[i][r].transform.localPosition *= multiplier;
+
+                if (adjustmentAngle.y != 0) { //making a turn, extend outside curves to accomidate
+                    if (i != 1) { //in this case, 1 is the inside rail (doing just zero for now as a test, in the final version it should be based on if the angle is negative or positive)
+                        //get full offset compared to rails[1]
+                        float offset = Mathf.Abs(railParents[1].transform.position.x) + Mathf.Abs(railParents[i].transform.position.x);
+
+                        //radius of the inside circle
+                        float radius1 = Mathf.Abs(sizes[i]);
+                        //radius of outside circle (rails[i])
+                        float radius2 = radius1 + offset;
+
+                        print(radius1 + "  " + radius2 + "    " + radius2/radius1);
+
+                        //rails[i][r].transform.localPosition *= radius2/radius1;
+                    }
+                }
             }
 
         }
