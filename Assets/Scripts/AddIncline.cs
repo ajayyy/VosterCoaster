82c5980fb028c1c17404c47dbdac9cae5f0b45b2
= <ajay@ajayinkingston.com>
2018-06-14 18:57:57 -0400

    moved track adjusting to a function.

--- Assets/Scripts/AddIncline.cs
+++ Assets/Scripts/AddIncline.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class AddIncline : MonoBehaviour {
 
-    //an array that contains arrays of each joint on the rails (maybe move rails to it's own class in the future)
-    GameObject[][] rails = new GameObject[3][];
-
     //array of all of the parents of the rail bones. This will be set by the inspector
     public GameObject[] railParents = new GameObject[3];
 
-    Vector3 adjustmentAngle = new Vector3(0, 3, 0); //the number in the x represents the total angle the whole track rotates divided by 9 (first bone does not have an angle
-
     void Start() {
+        AdjustTrack(new Vector3(0, 5, 0));
+    }
+
+    void Update () {
+
+    }
+
+    //adjustment angle: the number represents the total angle the whole track rotates divided by 9 (first bone does not have an angle
+    public void AdjustTrack(Vector3 adjustmentAngle) {
+        //an array that contains arrays of each joint on the rails (maybe move rails to it's own class in the future)
+        GameObject[][] rails = new GameObject[3][];
+
         //create the rails array from the railParents
 
         //original sizes (used for scaling)
@@ -52,7 +59,7 @@ public class AddIncline : MonoBehaviour {
                 Vector3 pos = rails[i][r].transform.position;
 
                 rails[i][r].transform.localPosition *= multiplier;
-                if(r == rails[i].Length - 2) {
+                if (r == rails[i].Length - 2) {
                     rails[i][r].transform.localPosition *= 2;
                 }
 
@@ -74,7 +81,7 @@ public class AddIncline : MonoBehaviour {
                         //radius of inside circle (rails[i])
                         float radius2 = radius1 - offset;
 
-                        print(radius1 + "  " + radius2 + "    " + radius2/radius1);
+                        print(radius1 + "  " + radius2 + "    " + radius2 / radius1);
 
                         rails[i][r].transform.localPosition *= radius2 / radius1;
                     }
@@ -83,8 +90,4 @@ public class AddIncline : MonoBehaviour {
 
         }
     }
-
-    void Update () {
-
-	}
 }
