2c5e172b54bc189f28f454a66ee1103069df509c
= <ajay@ajayinkingston.com>
2018-06-14 20:08:13 -0400

    added reset track function, now can adjust track on the fly and reset it

--- Assets/Scripts/AddIncline.cs
+++ Assets/Scripts/AddIncline.cs
@@ -7,12 +7,20 @@ public class AddIncline : MonoBehaviour {
     //array of all of the parents of the rail bones. This will be set by the inspector
     public GameObject[] railParents = new GameObject[3];
 
+    //variable that stores the default distance between bone points, used to reset the meshes
+    Vector3 defaultBonePosition = new Vector3(0, 0, -0.402642f);
+
+    float x = 1;
+
     void Start() {
-        AdjustTrack(new Vector3(0, 5, 0));
+        
     }
 
     void Update () {
+        ResetTrack();
+        AdjustTrack(new Vector3(0, x, 0));
 
+        x += 1 * Time.deltaTime;
     }
 
     //adjustment angle: the number represents the total angle the whole track rotates divided by 9 (first bone does not have an angle
@@ -89,5 +97,34 @@ public class AddIncline : MonoBehaviour {
             }
 
         }
+    }
+
+    public void ResetTrack() {
+        //an array that contains arrays of each joint on the rails (maybe move rails to it's own class in the future)
+        GameObject[][] rails = new GameObject[3][];
+
+        //go through all the bones in each rail parent and reset their position and rotation
+        for (int i = 0; i < railParents.Length; i++) {
+
+            //every iteration, parent is set to the next object in the hierchy to get the next child
+            GameObject parent = railParents[i];
+
+            for (int b = 0; b < 11; b++) {
+                GameObject bone = parent.transform.GetChild(0).gameObject;
+
+                parent = bone;
+
+                //reset this bone
+                bone.transform.localEulerAngles = Vector3.zero;
+                bone.transform.localPosition = defaultBonePosition;
+
+                //bone 0 has no offset since it is the start of the object
+                if (b == 0) {
+                    bone.transform.localPosition = Vector3.zero;
+                }
+            }
+
+        }
+
     }
 }
