3a91759d17083ac8ecdb06f691bed3b125af3b6c
= <ajay@ajayinkingston.com>
2018-08-15 13:48:08 -0400

    calculates the position on the circle for the person touching the touchpad

---
+++ Assets/Scripts/UI/ModeSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeSelector : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        GameController gameController = GameController.instance;
+
+        float vertical = Input.GetAxis("RightTrackpadVertical");
+        float horizontal = Input.GetAxis("RightTrackpadHorizontal");
+
+        //find position on a circle perimeter (angle)
+        float position = Mathf.Rad2Deg * Mathf.Atan2(vertical, horizontal);
+
+
+    }
+}
