97090da3ae87a576726b0db720f96cab99348c94
= <ajay@ajayinkingston.com>
2018-08-16 20:00:43 -0400

    Made the selected option grow when the finger is close to it. (radial toggle menu)

--- Assets/Scripts/UI/ModeSelector.cs
+++ Assets/Scripts/UI/ModeSelector.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ModeSelector : MonoBehaviour {
 
+    public RadialToggle[] buttons;
+
+    float defaultSize = 0.02f;
+    float maxSize = 0.03f;
+
 	void Start () {
 		
 	}
@@ -11,12 +16,42 @@ public class ModeSelector : MonoBehaviour {
 	void Update () {
         GameController gameController = GameController.instance;
 
-        float vertical = Input.GetAxis("RightTrackpadVertical");
-        float horizontal = Input.GetAxis("RightTrackpadHorizontal");
+        float vertical = gameController.rightController.GetAxis().y;
+        float horizontal = gameController.rightController.GetAxis().x;
 
-        //find position on a circle perimeter (angle)
-        float position = Mathf.Rad2Deg * Mathf.Atan2(vertical, horizontal);
+        //find position on a circle perimeter(angle)
+        float position = Mathf.Rad2Deg * Mathf.Atan2(vertical, horizontal) + 180;
 
+        //position += 1f;
+        //if (position > 360) {
+        //    position = 0;
+        //}
 
+        SetButtonSizes(position);
+    }
+
+    public void SetButtonSizes(float position) {
+        float[] positions = new float[buttons.Length];
+
+        for (int i = 0; i < positions.Length; i++) {
+            positions[i] = (360 / buttons.Length) * i;
+        }
+
+        for (int i = 0; i < buttons.Length; i++) {
+
+            float difference = Mathf.Abs(position - positions[i]);
+
+            //make sure it is the actual difference (difference between 359 and 0 is 1 not 359)
+            if (difference > 180) {
+                difference = 360 - difference;
+            }
+            if (difference < 90) {
+                float size = defaultSize + (maxSize - defaultSize) * (1 - difference / 90);
+
+                buttons[i].rectTransform.sizeDelta = new Vector2(1, 1) * size;
+            } else {
+                buttons[i].rectTransform.sizeDelta = new Vector2(1, 1) * defaultSize;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/UI/RadialToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+//This button will sit outside of the controller and be enabled/disabled when the trackpad is clicked and the item is selected
+public class RadialToggle : MonoBehaviour {
+
+    public bool optionEnabled;
+
+    public Image image;
+    public RectTransform rectTransform;
+
+    void Start() {
+        image = GetComponent<Image>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update() {
+
+    }
+}
